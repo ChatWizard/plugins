bc10b07af0b95c59f35e19c29fe1ec648ceef6f4
LisiurDay <lisiurday@gmail.com>
2023-04-22 22:19:16 +0800

    init

---
+++ plugins/commit-summary/src/commit_summary.rs
@@ -0,0 +1,36 @@
+wit_bindgen::generate!({
+    path: "../../wit"
+});
+
+struct MyHost;
+
+impl Host for MyHost {
+    fn run() {
+        let (status, output) = host_exec("git diff --cached");
+        if status != 0 {
+            println!("Error: {}", output);
+            return;
+        }
+
+        host_loading(true);
+
+        let prompt = r#"Suggest a few more commit messages for my changes (without explanations) following conventional commit (<type>: <subject>). I will give you the diff text and you give me the results as a list, not more than 6 items"#;
+        let prompt = format!("{prompt}\n {output}");
+        let (code, result) = host_openai(&prompt);
+
+        host_loading(false);
+
+        let options = result
+            .lines()
+            .map(|opt| opt.split_once('.').unwrap().1.trim())
+            .collect::<Vec<&str>>();
+
+        let result = host_select(&options);
+        if let Some(result) = result {
+            // host_exec(&format!("git commit -m \"{}\"", result));
+            println!("Code: {}\nResult: {}", code, result);
+        }
+    }
+}
+
+export_host!(MyHost);
